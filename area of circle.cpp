705cee91bbf5e21b718d60785b98626d4d8eaa6e
Md Kawsar Ahmed <153387694+kawsarahmed56@users.noreply.github.com>
2023-12-10 00:21:59 +0600

    solution for finding  the area of a circle

---
+++ area of circle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    float area,r;
+
+    cout << "enter the redius:";
+    cin>> r;
+
+    area = 3.1416*r*r;
+    cout<<"enter the area of circle:" << area;
+    getch();
+    return 0;
+}
