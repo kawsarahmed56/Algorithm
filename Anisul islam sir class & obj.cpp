0908ddfd3cfe1578aa2647818d9e02a0b56b3999
Md Kawsar Ahmed <153387694+kawsarahmed56@users.noreply.github.com>
2023-12-10 00:18:03 +0600

    added the solution of class and object

---
+++ Anisul islam sir class & obj.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class student{
+    public:
+int id;
+double cgpa;
+
+
+};
+
+int main(){
+
+student alim;
+alim.id=101;
+alim.cgpa=3.21;
+
+cout<< alim.id <<"  " << alim.cgpa;
+
+getch();
+
+}
+
+
+
